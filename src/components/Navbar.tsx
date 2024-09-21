e86e0e9e96023640096a13512ad79a7da13e7830
BrotherBaner <25873736+ABusyHippie@users.noreply.github.com>
2024-09-21 18:49:10 +0800

    Load yield trader by default

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 export default function Navbar(): JSX.Element {
   return (
     <nav className="sticky top-4 flex items-center justify-between py-3 px-5 rounded-full mt-4 w-full max-w-[1200px] mx-auto bg-gray-600/20 backdrop-blur-lg z-[100]">
-      <Link to="/">
+      <Link to="/home">
         <Logo className="w-[150px] h-[50px]" />
       </Link>
-      <Link to="/yield-trading" className="text-white hover:text-gray-300">
+      <Link to="/" className="text-white hover:text-gray-300">
         Yield Trading
       </Link>
       <ConnectButton
